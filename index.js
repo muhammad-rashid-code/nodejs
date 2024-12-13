71251aa08ec3950375d00f6dbee8f235aed3d3b2
Muhammad Rashid <m.rashid212823@gmail.com>
2024-12-14 03:17:02 +0500

    server chal gaya .gitignore file ban gye hai

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  console.log("req=>", req);
+  res.send("Hello BackEnd First Server! and API");
+});
+
+app.listen(PORT, () => console.log("Sever is Runnnig at PORT " + PORT));
