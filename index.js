5f03a4e0460335b6bc5f85898e8764117cf7eb1a
Muhammad Rashid <m.rashid212823@gmail.com>
2024-12-14 05:05:47 +0500

    HTTp request praticed using POSTMAN

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const PORT = 4000;
 
 app.get("/", (req, res) => {
   console.log("req=>", req);
-  res.send("Hello BackEnd First Server! and API");
+  res.send("Hello BackEnd First Server! and API This Is Get Request");
+});
+
+app.post("/", (req, res) => {
+  console.log("req=>", req);
+  res.send("Hello BackEnd First Server! and API This Is Post Request");
+});
+
+app.put("/", (req, res) => {
+  console.log("req=>", req);
+  res.send("Hello BackEnd First Server! and API This Is Put Request");
 });
 
 app.listen(PORT, () => console.log("Sever is Runnnig at PORT " + PORT));
